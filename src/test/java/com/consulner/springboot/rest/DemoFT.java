2e0b6389d844dbe0d4762e956720a737f84cba3f
Marcin Piczkowski <marcin.piczkowski@aurea.com>
2017-10-08 20:47:33 +0200

    parametrized tomcat host in junit test, property from maven or passed in -Dtomcat.host variable is taken

--- src/test/java/com/consulner/springboot/rest/DemoFT.java
+++ src/test/java/com/consulner/springboot/rest/DemoFT.java
@@ -1,6 +1,10 @@
 package com.consulner.springboot.rest;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 import org.json.JSONException;
+import org.junit.Before;
 import org.junit.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.http.HttpEntity;
@@ -12,29 +16,44 @@ import org.springframework.web.client.RestTemplate;
 public class DemoFT {
 
   private static final String port = "8080";
-  private static  final String appContext = "/RestDemo-0.0.1-SNAPSHOT";
+  private static final String appContext = "/RestDemo-0.0.1-SNAPSHOT";
 
-  RestTemplate restTemplate = new RestTemplate();
+  private RestTemplate restTemplate = new RestTemplate();
 
-  HttpHeaders headers = new HttpHeaders();
+  private HttpHeaders headers = new HttpHeaders();
+
+  private Properties prop = new Properties();
+
+  @Before
+  public void setup() throws IOException {
+    InputStream stream = getClass().getResourceAsStream("/test.properties");
+    prop.load(stream);
+  }
+
 
   @Test
   public void testRetrieveStudentCourse() throws JSONException {
 
-//    HttpEntity<String> entity = new HttpEntity<String>(null, headers);
-//
-//    ResponseEntity<String> response = restTemplate.exchange(
-//        createURLWithPort("/demo"),
-//        HttpMethod.GET, entity, String.class);
-//
-//    String expected = "{\"message\":\"Hello there!!\"}";
-//    String resp = response.getBody();
-//    System.out.println(resp);
-//    JSONAssert.assertEquals(expected,resp , false);
+    HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+
+    ResponseEntity<String> response = restTemplate.exchange(
+        createURLWithPort("/demo"),
+        HttpMethod.GET, entity, String.class);
+
+    String expected = "{\"message\":\"Hello there!!\"}";
+    String resp = response.getBody();
+    System.out.println(resp);
+    JSONAssert.assertEquals(expected, resp, false);
   }
 
   private String createURLWithPort(String uri) {
-    return "http://172.17.0.1:" + port + appContext +  uri;
+    System.out.println(prop.stringPropertyNames());
+    System.out.println(prop.getProperty("tomcat.host"));
+    return String.format("http://%s:%s/%s/%s",
+            prop.getProperty("tomcat.host"),
+            port,
+            appContext,
+            uri);
   }
 }
 
